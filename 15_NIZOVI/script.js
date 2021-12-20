74d70ed63713f7dd8148090bd2f3f5e1d75d8e0a
MarijaScekicZivojinovic <mladjoscekic@gmail.com>
2021-12-20 03:22:33 +0100

    do 24og zadatka

--- 15_NIZOVI/script.js
+++ 15_NIZOVI/script.js
@@ -234,9 +234,9 @@ console.log(min);
 
 let indexMax = celobrojniNiz => celobrojniNiz.indexOf(najveci(celobrojniNiz)); //ili indexOf(max)
 
-console.log("Index maksimalne vrednosti u prvom nizu je " + indexMax(celobrojniNiz));
-console.log("Index maksimalne vrednosti u drugom nizu je " + indexMax(niz2));
-console.log("Index maksimalne vrednosti u trecem nizu je " + indexMax(niz3));
+console.log("Zadatak 7: Index maksimalne vrednosti u prvom nizu je " + indexMax(celobrojniNiz));
+console.log("Zadatak 7: Index maksimalne vrednosti u drugom nizu je " + indexMax(niz2));
+console.log("Zadatak 7: Index maksimalne vrednosti u trecem nizu je " + indexMax(niz3));
 
 //drugi nacin 
 
@@ -251,7 +251,7 @@ let indexMaxVr = celobrojniNiz => {
     }
     return memorisi;
 }
-console.log("testiranje: " + indexMaxVr(celobrojniNiz));
+console.log("Zadatak 7: testiranje: " + indexMaxVr(celobrojniNiz));
 
 /* zadatak 8: Odrediti indeks minimalnog elementa celobrojnog niza.*/
 /* podsetnik: 
@@ -260,9 +260,9 @@ niz2 = [6, -9, 0, 0, 3];
 niz3 = [1, 4];
 */
 let indexMin = celobrojniNiz => celobrojniNiz.indexOf(najmanji(celobrojniNiz));
-console.log("Index minimalne vrednosti u prvom nizu je " + indexMin(celobrojniNiz));
-console.log("Index minimalne vrednosti u drugom nizu je " + indexMin(niz2));
-console.log("Index minimalne vrednosti u trecem nizu je " + indexMin(niz3));
+console.log("Zadatak 8: Index minimalne vrednosti u prvom nizu je " + indexMin(celobrojniNiz));
+console.log("Zadatak 8: Index minimalne vrednosti u drugom nizu je " + indexMin(niz2));
+console.log("Zadatak 8: Index minimalne vrednosti u trecem nizu je " + indexMin(niz3));
 
 // drugi nacin
 
@@ -277,7 +277,7 @@ let indexMinVr = celobrojniNiz => {
     }
     return memorisi;
 }
-console.log("testiranje 2: " + indexMinVr(celobrojniNiz));
+console.log("Zadatak 8: testiranje 2: " + indexMinVr(celobrojniNiz));
 
 /* zadatak 9: Odrediti broj elemenata celobrojnog niza koji su veći od srednje vrednosti.*/
 
@@ -290,7 +290,7 @@ let veceOdSredine = celobrojniNiz => {
     }
     return br;
 }
-console.log("Broj brojeva u nizu vecih od aritmeticke sredine iznosi " + veceOdSredine(celobrojniNiz));
+console.log("Zadatak 9: Broj brojeva u nizu vecih od aritmeticke sredine iznosi " + veceOdSredine(celobrojniNiz));
 /* podsetnik: 
 celobrojniNiz = [1, 4, 4, 9, 7, 8]; 
 arSr3 = 5.5
@@ -307,7 +307,7 @@ let pozitivniEl = celobrojniNiz => {
     }
     return suma;
 }
-console.log("Suma pozitivnih elemenata u prvom celobrojnom nizu iznosi " + pozitivniEl(celobrojniNiz));
+console.log("Zadatak 10: Suma pozitivnih elemenata u prvom celobrojnom nizu iznosi " + pozitivniEl(celobrojniNiz));
 
 /* zadatak 11: Odrediti broj parnih elemenata u celobrojnom nizu.
 */
@@ -322,23 +322,195 @@ let parni = celobrojniNiz => {
     return br;
 }
 
-console.log("Broj parnih elemenata u prvom nizu iznosi " + parni(celobrojniNiz));
+console.log("Zadatak 11: Broj parnih elemenata u prvom nizu iznosi " + parni(celobrojniNiz));
 
 // zadatak 12: Odrediti broj parnih elemenata sa neparnim indeksom.
 
+let neparniIndexParniEl = celobrojniNiz => {
+    let br = 0;
+    for (i = 0; i < celobrojniNiz.length; i++) {
+        if (celobrojniNiz[i] % 2 == 0 && i % 2 != 0) {
+            br++;
+        }
+    }
+    return br;
+}
+
+console.log("Zadatak 12: Broj parnih elemenata sa neparnim indexom u prvom nizu iznosi " + neparniIndexParniEl(celobrojniNiz));
+
 //zadatak 13: Izračunati sumu elemenata niza sa parnim indeksom.
+
+/* podsetnik: 
+celobrojniNiz = [1, 4, 4, 9, 7, 8]; 
+arSr3 = 5.5
+*/
+
+let parniIndexSuma = celobrojniNiz => {
+    let suma = 0;
+    for (i = 0; i < celobrojniNiz.length; i++) {
+        if (i % 2 == 0) {
+            suma += celobrojniNiz[i];
+        }
+    }
+    return suma;
+}
+
+console.log("Zadatak 13: Suma elemenata sa parnim indexom (zero included) u prvom nizu iznosi " + parniIndexSuma(celobrojniNiz));
+
 //zadatak 14: Promeniti znak svakom elementu celobrojnog niza.
+
+let MenjaZnak = niz => {
+    for (let i = 0; i < niz.length; i++) {
+        console.log(-1 * niz[i]);
+    }
+}
+MenjaZnak(celobrojniNiz);
+
+/* podsetnik: 
+celobrojniNiz = [1, 4, 4, 9, 7, 8]; 
+arSr3 = 5.5
+*/
+
 //zadatak 15: Promeniti znak svakom neparnom elementu celobrojnog niza sa parnim indeksom.
 
+let MenjaZnak2 = niz => {
+    for (let i = 0; i < niz.length; i++) {
+        if (i % 2 == 0 && niz[i] % 2 != 0) {
+            //ili da ispise 0 - niz[i];
+            console.log(-1 * niz[i]);
+        }
+    }
+}
+MenjaZnak2(celobrojniNiz);
+
 //zadatak 16: Dat je niz stavki za kupovinu (članovi niza su stringovi). Prolaskom kroz niz napraviti neuređenu listu i ispisati je u html dokument.
+
+let shopping = ["mleko", "burek", "cokolada", "jogurt"];
+let lista = shopping => {
+    let tabela1 = `<ul style="border: 1px solid black; width: 50%; padding: 5px; list-style-position: inside">`;
+    for (i = 0; i <= shopping.length; i++) {
+        tabela1 += `<li>${shopping[i]}</li>`;
+    }
+    tabela1 += `</ul>`;
+    return document.body.innerHTML += tabela1;
+}
+console.log("Zadatak 16 je na ekranu - lista.");
+lista(shopping);
+
 // zadatak 17: Dat je niz imena košarkaškog tima. Prolaskom kroz niz formirati tabelu u čijim su redovima imena tima, i tabelu ispisati u html dokument.
-// zadatak 18: Dat je niz stringova čiji su članovi putanje do slika. Prikazati sve sliku u html dokumentu sa putanjama navedenim u nizu.
+
+let kosarka = ["Djordje The Boss", "Boxa The Boss", "Marija The Boss"];
+
+let tabela = kosarka => {
+    let tabela2 = `<table style="border: 1px solid black; width: 50%; padding: 5px; ">`;
+    for (i = 0; i < kosarka.length; i++) {
+        tabela2 += `<tr><td>${[i + 1]}. ${kosarka[i]}</td></tr>`;
+    }
+    tabela2 += `</table>`;
+    return document.body.innerHTML += tabela2;
+}
+tabela(kosarka);
+console.log("Zadatak 17 je na ekranu - tabela.");
+
+// zadatak 18: Dat je niz stringova čiji su članovi putanje do slika. Prikazati svaku sliku u html dokumentu sa putanjama navedenim u nizu.
+
+let slike1 = ["https://media.distractify.com/brand-img/NGZNE1pEd/0x0/gojo-satoru-1621289028724.png", "https://static1.cbrimages.com/wordpress/wp-content/uploads/2021/10/Gojo-Jujutsu-Kaisen-Eyes-Uncovered.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5", "https://animehunch.com/wp-content/uploads/2021/06/a412b1344e2a1bf02c273854edc370ea-768x437.jpg"];
+
+let putanjeSlika = slike1 => {
+    for (i = 0; i < slike1.length; i++) {
+        document.body.innerHTML += `<img src=${slike1[i]} width=200>`;
+    }
+}
+
+putanjeSlika(slike1);
+console.log("Zadatak 18 je na ekranu - slike.");
+
 // zadatak 19: Ispisati dužinu svakog elementa u nizu stringova.
+
+let string1 = ["Marija", "Djordje", "Boxa"];
+
+let duzinaElementa = string1 => {
+    for (i = 0; i < string1.length; i++) {
+        console.log(string1[i].length);
+    }
+}
+duzinaElementa(string1);
+
 //zadatak 20: Odrediti element u nizu stringova sa najvećom dužinom.
+
+let string0 = ["Marija", "Djordje", "Boxa", "Bogdan", "Nikola", "Leo", "Som", "Haralampije", "Tea", "Mia", "Tatjana", "Cica"];
+
+let MaxDuzinaElementa = string0 => {
+    let max = string0[0].length; //pretpostavka
+    // console.log(max);
+    let zapamti;
+    for (i = 1; i < string0.length; i++) { //i=1 jer se proverava naredni element, ne [0]0], imali bi korak viska da smo stavili i=0
+        if (string0[i].length > max) {
+            max = string0[i].length;
+            zapamti = string0[i];
+        }
+    }
+    return zapamti;
+}
+console.log("Zadatak 20: " + MaxDuzinaElementa(string0));
+
 // zadatak 21: Odrediti broj elemenata u nizu stringova čija je dužina veća od prosečne dužine svih stringova u nizu.
+
+// prosečne dužine svih stringova u nizu. -> aritmeticka sredina
+let arSredinaDuzine = niz => {
+    let suma = 0;
+    let brojac = 0;
+    for (let i = 0; i < niz.length; i++) {
+        suma += niz[i].length;
+        brojac++;
+    }
+    return (brojac != 0) ? suma / brojac : 0;
+}
+
+let duzinaIznadProseka = niz => {
+    let brojac = 0;
+    for (let i = 0; i < niz.length; i++) {
+        if (niz[i].length > arSredinaDuzine(niz)) {
+            brojac++;
+        }
+    }
+    return brojac;
+}
+
+console.log("Zadatak 21: " + duzinaIznadProseka(string0));
+
 //zadatak 22: Odrediti broj elemenata u nizu stringova koji sadrže slovo 'a’.
 
-//zadatak 23: Odrediti broj elemenata u nizu stringova koji počinju na slovo 'a' ili 'A’. 
+let string2 = ["Marija", "Djordje", "Boxa", "Bogdan", "Nikola", "Leo", "Som"];
+
+let sadrziSlovoA = niz => {
+    let brojac = 0;
+    for (let i = 0; i < niz.length; i++) {
+        if (niz[i].includes("a")) {
+            brojac++;
+        }
+    }
+    console.log("Zadatak 22: " + brojac);
+}
+
+sadrziSlovoA(string2);
+
+//zadatak 23: Odrediti broj elemenata u nizu stringova koji počinju na slovo 'a' ili 'A’.
+
+let pocinjeNaA = ["Marija", "Boxa", "Andrija", "Andjela", "Djordje", "I am legend", "avokado", "banana", "uananananananana"];
+
+let prvoSlovoA = niz => {
+    let brojac = 0;
+    for (let i = 0; i < niz.length; i++) {
+        if (niz[i].charAt(0) === "a" || niz[i].charAt(0) === "A") {
+            console.log(niz[i]);
+            brojac++;
+        }
+    }
+    console.log(brojac);
+}
+
+prvoSlovoA(pocinjeNaA);
 
 /*zadatak 24: Dati su nizovi
 a[0],a[1],...a[n-1] i
