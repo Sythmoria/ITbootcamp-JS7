2e9ca878c059ad0b849e1bb4b5419c763c3cb716
MarijaScekicZivojinovic <mladjoscekic@gmail.com>
2022-01-14 03:15:05 +0100

    nastavak filmova

--- 22_KLASE/film/film.js
+++ 22_KLASE/film/film.js
@@ -3,6 +3,21 @@
 // Testirati metode klase.
 // U klasi Film, dodati po tri setera za sva polja, s tim da se u seteru za godinu izdanja proverava da li je godina veća od 1800. Ukoliko godina izdanja nije veća od 1800, godinu setovati na 1800.
 
+// U klasi Film dodati polje ocene koje čini niz ocena koje su korisnici dali filmu
+// Kreirati niz od barem tri objekta klase Film
+// Napraviti metod prosek koji vraća prosečnu ocenu 
+// Napraviti funkciju vekFilmova kojoj se prosleđuje niz filmova i ceo broj (vek), a funkcija ispisuje samo one filmove koji su stvoreni u prosleđenom veku
+// Napraviti funkciju prosecnaOcena kojoj se prosleđuje niz filmova, a koja određuje i vraća  prosečnu ocenu svih filmova.
+// Napraviti funkciju najboljeOcenjeni kojoj se prosleđuje niz filmova, a ona vraća najbolje ocenjeni film.
+// Napraviti funkciju osrednjiFilm kojoj se prosleđuje niz filmova a ona vraća film koji je najbliži prosečnoj oceni svih filmova.
+// Napraviti funkciju najmanjaOcenaNajboljeg kojoj se prosleđuje niz filmova a ona određuje najbolji film i ispisuje njegovu najslabiju ocenu.
+// Napisati funkciju najmanjaOcena kojoj se prosleđuje niz filmova, a koja vraća koja je najmanja ocena koju je bilo koji film dobio.
+// Napisati funkciju najcescaOcena kojoj se prosleđuje niz ocena, a ona vraća ocenu koju su filmovi najčešće dobijali. 
+// Napraviti funkciju iznadOcene kojoj se prosleđuje ocena i niz filmova, a ona vraća niz onih filmova koji su bolje ocenjeni od prosleđene ocene.
+// Napisati funkciju iznadOceneNoviji kojoj se prosleđuje ocena i niz filmova  a koja treba da na ekranu da ispiše sve podatke o najnovijem filmu koji zadovoljava prosleđenu ocenu
+
+
+
 
 export class Film {
     constructor(a, b, c, d) { //d je niz ocena
@@ -68,13 +83,22 @@ export class Film {
 
     //DODAVANJE JEDNE OCENE
     dodajOcenu(novaOcena) {
-        this._ocene.push(novaOcena); //ne treba nam seter i geter
+        this._ocene.push(novaOcena); //ne treba nam seter i geter, u geteru je return ovoga sto ovde ispisujemo 
     }
 
     //OSTALE METODE
     stampaj() {
         // console.log(this.naslov);
         console.log(this);
+    }
+
+    // Napraviti metod prosek koji vraća prosečnu ocenu 
+    prosecnaOcena() {
+        let suma = 0;
+        this.ocene.forEach(element => {
+            suma += element;
+        });
+        return (this.ocene.length != 0) ? suma / this.ocene.length : 0;
     }
 }
 
--- 22_KLASE/film/script.js
+++ 22_KLASE/film/script.js
@@ -1,9 +1,9 @@
 import { Film } from "./film.js"; //posto nije default u film.js onda ovde Film ubacujemo u viticaste zagrade
 
-let film1 = new Film("Inception", "Christopher Nolan", 1789, [8.2, 9.3, 7.9]);
+let film1 = new Film("A Beautiful Mind", "Ron Howard", 2001, [8.2, 9.3, 7.9]);
 // console.log(film1);
 film1.stampaj();
-let film2 = new Film("Stari film", "nepoznati autor", 1801.5, [8, 9, 7, 6]);
+let film2 = new Film("Parasite", "Bong Joon Ho", 2019, [8, 9, 7, 6]);
 // console.log(film2);
 film2.stampaj();
 let film3 = new Film("Princess Mononoke", "Hayao Miyazaki", 1997, [7, 6, 7, 7, 8, 7]);
@@ -11,11 +11,26 @@ let film3 = new Film("Princess Mononoke", "Hayao Miyazaki", 1997, [7, 6, 7, 7, 8
 film3.stampaj();
 
 //testiram da li seter za naslov radi:
-film1.naslov = "Inception 1";
+film1.naslov = "A Beautiful Mind <3";
 film1.stampaj();
 
 //testiram da li geter za naslov radi:
 console.log(film1.reziser);
+
+
+// U klasi Film dodati polje ocene koje čini niz ocena koje su korisnici dali filmu
+// Kreirati niz od barem tri objekta klase Film
+// Napraviti metod prosek koji vraća prosečnu ocenu 
+
+// Napraviti funkciju vekFilmova kojoj se prosleđuje niz filmova i ceo broj (vek), a funkcija ispisuje samo one filmove koji su stvoreni u prosleđenom veku
+// Napraviti funkciju prosecnaOcena kojoj se prosleđuje niz filmova, a koja određuje i vraća  prosečnu ocenu svih filmova.
+// Napraviti funkciju najboljeOcenjeni kojoj se prosleđuje niz filmova, a ona vraća najbolje ocenjeni film.
+// Napraviti funkciju osrednjiFilm kojoj se prosleđuje niz filmova a ona vraća film koji je najbliži prosečnoj oceni svih filmova.
+// Napraviti funkciju najmanjaOcenaNajboljeg kojoj se prosleđuje niz filmova a ona određuje najbolji film i ispisuje njegovu najslabiju ocenu.
+// Napisati funkciju najmanjaOcena kojoj se prosleđuje niz filmova, a koja vraća koja je najmanja ocena koju je bilo koji film dobio.
+// Napisati funkciju najcescaOcena kojoj se prosleđuje niz ocena, a ona vraća ocenu koju su filmovi najčešće dobijali. 
+// Napraviti funkciju iznadOcene kojoj se prosleđuje ocena i niz filmova, a ona vraća niz onih filmova koji su bolje ocenjeni od prosleđene ocene.
+// Napisati funkciju iznadOceneNoviji kojoj se prosleđuje ocena i niz filmova  a koja treba da na ekranu da ispiše sve podatke o najnovijem filmu koji zadovoljava prosleđenu ocenu
 
 //dodavanje ocene u film1:
 film1.dodajOcenu(10);
@@ -43,3 +58,41 @@ filmovi.forEach(element => {
 });
 tabelica += `</table>`;
 document.body.innerHTML = tabelica;
+
+// Napraviti metod prosek koji vraća prosečnu ocenu 
+
+console.log(film1.prosecnaOcena());
+console.log(film2.prosecnaOcena());
+
+// Napraviti funkciju vekFilmova kojoj se prosleđuje niz filmova i ceo broj (vek), a funkcija ispisuje samo one filmove koji su stvoreni u prosleđenom veku
+
+// iz godine u vek: Math.floor((year-1)/100) + 1;
+
+function vekFilmova(array, century) {
+    if (Math.floor(century) != century) {
+        century = Math.floor(century);
+    }
+    array.forEach(element => {
+        if (element.godinaIzdanja >= ((century - 1) * 100 + 1) && element.godinaIzdanja <= century * 100) {
+            console.log(element);
+        }
+    })
+}
+
+vekFilmova(filmovi, 21);
+
+// Napraviti funkciju prosecnaOcena kojoj se prosleđuje niz filmova, a koja određuje i vraća  prosečnu ocenu svih filmova.
+
+// Napraviti funkciju najboljeOcenjeni kojoj se prosleđuje niz filmova, a ona vraća najbolje ocenjeni film.
+
+// Napraviti funkciju osrednjiFilm kojoj se prosleđuje niz filmova a ona vraća film koji je najbliži prosečnoj oceni svih filmova.
+
+// Napraviti funkciju najmanjaOcenaNajboljeg kojoj se prosleđuje niz filmova a ona određuje najbolji film i ispisuje njegovu najslabiju ocenu.
+
+// Napisati funkciju najmanjaOcena kojoj se prosleđuje niz filmova, a koja vraća koja je najmanja ocena koju je bilo koji film dobio.
+
+// Napisati funkciju najcescaOcena kojoj se prosleđuje niz ocena, a ona vraća ocenu koju su filmovi najčešće dobijali.
+
+// Napraviti funkciju iznadOcene kojoj se prosleđuje ocena i niz filmova, a ona vraća niz onih filmova koji su bolje ocenjeni od prosleđene ocene.
+
+// Napisati funkciju iznadOceneNoviji kojoj se prosleđuje ocena i niz filmova  a koja treba da na ekranu da ispiše sve 
