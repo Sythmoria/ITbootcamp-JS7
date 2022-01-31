6146d3eb9e8e05db70a69ee521fe900d687dca0f
MarijaScekicZivojinovic <mladjoscekic@gmail.com>
2022-01-31 16:52:47 +0100

    TASK_04 kreiranje asinhronog metoda za dodavanje nove poruke u bazu podataka

--- 29_CHAT_PROJEKAT/chat.js
+++ 29_CHAT_PROJEKAT/chat.js
@@ -20,8 +20,8 @@ export class Chatroom {
     set username(un) {
         un = un.trim();
         if (un.length > 0) {
-            // let usernameRegex = /^[a-zA-Z0-9]+$/; //^ is added to ensure username starts with a letter
-            // let validUsername = un.match(usernameRegex);
+            // let usernameRegex = /^[a-zA-Z0-9]+$/; //^ is added to ensure username starts with a letter, and $ is added so that we could check the END of the string ; /^[a-zA-Z0-9\s'-]+$/ -> \s is for white spaces -> check this out
+            // usernameRegex(un) -> if it's true, it's valis
             // this._username = validUsername;
             this._username = un;
         }
@@ -32,5 +32,22 @@ export class Chatroom {
     get username() {
         return this._username;
     }
+
+    // async method
+    async addChat(message) {
+        let currentDate = new Date();
+
+        let chatTemplate = {
+            message: message,
+            username: this.username,
+            room: this.room,
+            createdAt: firebase.firestore.Timestamp.fromDate(currentDate)
+        }
+
+        //saving the document into the database
+        let response = await this.chats.add(chatTemplate); //we added await so we could wait for this to complete
+        return response; //returning Promise, and we can add then() and catch() for this response   
+    }
+
 }
 
--- 29_CHAT_PROJEKAT/script.js
+++ 29_CHAT_PROJEKAT/script.js
@@ -1,8 +1,13 @@
 import { Chatroom } from "./chat.js";
 
-let chatroom1 = new Chatroom("js", "Stefan");
+let chatroom1 = new Chatroom("js", "Stephen");
 console.log(chatroom1.username, chatroom1.room); //testing getters
 chatroom1.username = "Dusan";
 console.log(chatroom1.username, chatroom1.room);
 chatroom1.room = "general";
 console.log(chatroom1.room);
+
+let chatroom2 = new Chatroom("general", "Michelle");
+chatroom2.addChat("Wednesday HR training") //it returned Promise, so we can add .then() and .catch()
+    .then(() => console.log("Chat added successfully."))
+    .catch(error => console.log(`Error has occurred: ${error}`))
