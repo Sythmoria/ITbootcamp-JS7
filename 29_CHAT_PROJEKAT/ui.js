2aa9d327b8ef602fe656ab9540591ae2d774b5bc
MarijaScekicZivojinovic <mladjoscekic@gmail.com>
2022-02-01 16:32:24 +0100

    TASK_9 date format

--- 29_CHAT_PROJEKAT/ui.js
+++ 29_CHAT_PROJEKAT/ui.js
@@ -8,13 +8,33 @@ export class ChatUI {
     get list() {
         return this._list;
     }
+    // formatting the date to a more comprehensive format
+    formatDate(date) {
+        let day = date.getDate();
+        day = String(day).padStart(2, "0");
+        let month = (date.getMonth() + 1); //months start with 0, so we need to add 1
+        month = String(month).padStart(2, "0");
+        let year = date.getFullYear();
+        let hour = date.getHours();
+        hour = String(hour).padStart(2, "0");
+        let minutes = date.getMinutes();
+        minutes = String(minutes).padStart(2, "0");
+        let today = new Date();
+        if (date.getDate() === today.getDate()) {
+            return `${day}/${month}/${year}`;
+        }
+        else {
+            return `${day}/${month}/${year} ${hour}:${minutes}`;
+        }
+    }
     // adding a method for creating list items and print list items on the page 
     templateLI(docData) {
+        let date = docData.createdAt.toDate();
         let htmlLI =
             `<li>
         ${docData.username} : ${docData.message}
         <br>
-        ${docData.createdAt}
+        ${this.formatDate(date)}
         </li>`;
         this.list.innerHTML += htmlLI;
     }
