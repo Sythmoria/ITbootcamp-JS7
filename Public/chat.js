2267f9a805f5bcc424f401e27c8cbf1b720991b7
MarijaScekicZivojinovic <mladjoscekic@gmail.com>
2022-02-07 16:10:27 +0100

    Firebase

---
+++ Public/chat.js
@@ -0,0 +1,125 @@
+import { startValueFirebase, endValueFirebase } from "./app.js";
+
+export class Chatroom {
+    constructor(rm, un) {
+        this.room = rm;
+        this.username = un;
+        this.chats = db.collection('chats');
+        this.unsub = false;
+    }
+    // get and set for ROOM
+    set room(rm) {
+        if (rm === "general" || rm === "js" || rm === "homework" || rm === "tests") {
+            this._room = rm;
+        }
+        else {
+            this._room = "general";
+        }
+    }
+    get room() {
+        return this._room;
+    }
+    // get and set for USERNAME
+    set username(un) {
+        if (this.validateUsername(un) === true) {
+            this._username = un;
+        }
+        else {
+            alert("Please enter a valid username!");
+        }
+    }
+    get username() {
+        return this._username;
+    }
+
+    // validation for the username
+    validateUsername(a) {
+        a = a.trim();
+        if (a.length >= 2 && a.length <= 10 && /^[a-zA-Z0-9\s'-]+$/.test(a) === true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    updateUsername(newUsername) {
+        if (this.validateUsername(newUsername) === true) {
+            this.username = newUsername;
+            localStorage.setItem("username", newUsername);
+        }
+        else {
+            alert("Please enter a valid username!");
+        }
+    }
+
+    updateRoom(newRoom) {
+        this.room = newRoom;
+        if (this.unsub != false) {
+            this.unsub();
+        }
+    }
+
+    // async method
+    async addChat(message) {
+        let currentDate = new Date();
+        message = message.trim();
+        let chatTemplate;
+        if (message.length > 0) {
+            chatTemplate = {
+                message: message,
+                username: this.username,
+                room: this.room,
+                createdAt: firebase.firestore.Timestamp.fromDate(currentDate)
+            }
+        }
+        else {
+            alert(`You've tried sending an empty message`);
+        }
+        //saving the document into the database
+        let response = await this.chats.add(chatTemplate);
+        return response;
+    }
+
+    //Method that reacts to changes
+    getChats(callback) {
+        this.unsub = this.chats
+            .where("room", "==", this.room)
+            .orderBy("createdAt")
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type == "added") {
+                        callback(change.doc);
+                    }
+                });
+            });
+    }
+
+    //Method that erases messages
+    deleteMessage(id) {
+        this.chats
+            .doc(id)
+            .delete()
+            .then(() => {
+                console.log("Successfully deleted.");
+            })
+            .catch(error => {
+                console.log("An error has occurred " + error);
+            })
+    }
+
+    showMsgsFromTo(callback) {
+        this.chats
+            .where('createdAt', '>=', startValueFirebase)
+            .where('createdAt', '<=', endValueFirebase)
+            .where('room', '==', this.room)
+            .orderBy('createdAt')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type == 'added') {
+                        callback(change.doc);
+                    }
+                });
+            })
+    }
+}
---
+++ Public/ui.js
@@ -0,0 +1,62 @@
+export class ChatUI {
+    constructor(listItem) {
+        this.list = listItem;
+    }
+    set list(listItem) {
+        this._list = listItem;
+    }
+    get list() {
+        return this._list;
+    }
+    // formatting the date to a more comprehensive format
+    formatDate(date) {
+        let day = date.getDate();
+        day = String(day).padStart(2, "0");
+        let month = (date.getMonth() + 1); //months start with 0, so we need to add 1
+        month = String(month).padStart(2, "0");
+        let year = date.getFullYear();
+        let hour = date.getHours();
+        hour = String(hour).padStart(2, "0");
+        let minutes = date.getMinutes();
+        minutes = String(minutes).padStart(2, "0");
+        let today = new Date();
+        if (date.getDate() === today.getDate()) {
+            return `${day}/${month}/${year}`;
+        }
+        else {
+            return `${day}/${month}/${year} ${hour}:${minutes}`;
+        }
+    }
+    // adding a method for creating list items and print list items on the page 
+    templateLI(data) {
+        let id = data.id;
+        data = data.data();
+        let date = data.createdAt.toDate();
+        if (data.username == localStorage.username) {
+            let htmlLI =
+                `<li class="goRight" id="${id}">
+        <strong>${data.username}</strong> 
+        <br>
+        ${data.message}
+        <br>
+        ${this.formatDate(date)}  <i class="fa fa-trash-o" style="font-size:24px"></i>
+        </li>`;
+            this.list.innerHTML += htmlLI;
+        }
+        else {
+            let htmlLI =
+                `<li class="goLeft" id="${id}">
+                <strong>${data.username}</strong>
+            <br>
+            ${data.message}
+            <br>
+        ${this.formatDate(date)}  <i class="fa fa-trash-o" style="font-size:24px"></i>
+        </li>`;
+            this.list.innerHTML += htmlLI;
+        }
+    }
+
+    clearContent() {
+        this.list.innerHTML = ``;
+    }
+}
